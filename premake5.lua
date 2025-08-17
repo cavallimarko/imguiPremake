bbca386ecc382dc0ad3279e42e767508aca7bc3a
Marko Cavalli <cavalli.marko@gmail.com>
2025-08-17 19:56:13 +0200

    update to static lib

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,9 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++20"
+    staticruntime "On"
+
     
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -22,8 +25,11 @@ project "ImGui"
     
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++20"
-        staticruntime "On"
-        
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
