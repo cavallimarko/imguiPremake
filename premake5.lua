48f5f37e972d065b88b4178a5bc210b4fe6572ba
Marko Cavalli <cavalli.marko@gmail.com>
2025-10-20 20:17:36 +0200

    static urntime off

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "ImGui"
     kind "StaticLib"
     language "C++"
     cppdialect "C++20"
-    staticruntime "On"
+    staticruntime "off"
 
     
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
