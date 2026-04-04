f9c990f9988687961cfd1494d97d26fa8c827633
Marko Cavalli <cavalli.marko@gmail.com>
2026-04-04 17:36:29 +0200

    imgui dist

--- premake5.lua
+++ premake5.lua
@@ -4,9 +4,8 @@ project "ImGui"
     cppdialect "C++20"
     staticruntime "off"
 
-    
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
     files
     {
@@ -68,11 +67,19 @@ project "ImGui"
     
     filter "system:windows"
         systemversion "latest"
+        buildoptions
+        {
+            "/utf-8"
+        }
 
     filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
 	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
